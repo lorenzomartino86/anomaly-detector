1734343b2ca0df3069a4a17f256e170dec45e7e0
lore <lorenzomartino86@gmail.com>
2017-12-05 15:09:33 +0100

    added new test for cluster pipeline

--- test/domain/pipeline/TestClusterPipeline.py
+++ test/domain/pipeline/TestClusterPipeline.py
@@ -12,11 +12,14 @@ class TestClusterPipeline(unittest.TestCase):
     def test_no_new_cluster(self):
         train_raw = [RawInput(corpus="Hello world!"),
                      RawInput(corpus="Hello world!")]
-        test_raw = [RawInput(corpus="Hello world!")]
+        test_raw = [RawInput(corpus="Hello world!"),
+                    RawInput(corpus="Hello"),
+                    RawInput(corpus="world"),
+                    RawInput(corpus="Hello world")]
 
         detected_clusters = self.pipeline.detect(train_raw=train_raw, test_raw=test_raw)
 
-        self.assertEqual(len(detected_clusters), 0, "no cluster should be detected")
+        self.assertEqual(len(detected_clusters), 0, "any new cluster should be detected")
 
     def test_one_new_cluster(self):
         train_raw = [RawInput(corpus="Hello world!"),
@@ -25,7 +28,21 @@ class TestClusterPipeline(unittest.TestCase):
 
         detected_clusters = self.pipeline.detect(train_raw=train_raw, test_raw=test_raw)
 
-        self.assertEqual(len(detected_clusters), 1, "one cluster should be detected")
+        self.assertEqual(len(detected_clusters), 1, "one new cluster should be detected")
+
+
+    def test_multiple_new_clusters(self):
+        train_raw = [RawInput(corpus="Hello world!"),
+                     RawInput(corpus="Hello world!")]
+        test_raw = [RawInput(corpus="Jeepers creepers for ever and ever"),
+                    RawInput(corpus="Jeepers creepers"),
+                    RawInput(corpus="Mountain sea lake"),
+                    RawInput(corpus="star wars the revenge")]
+
+        detected_clusters = self.pipeline.detect(train_raw=train_raw, test_raw=test_raw)
+
+        self.assertEqual(len(detected_clusters), 3, "three new clusters should be detected")
+
 
 if __name__ == '__main__':
     unittest.main()
