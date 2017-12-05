88beeb62be536621317df068c141d187d001d66b
lore <lorenzomartino86@gmail.com>
2017-12-05 23:59:24 +0100

    fixed repository test

--- test/adapter/repository/TestFileRepository.py
+++ test/adapter/repository/TestFileRepository.py
@@ -9,7 +9,8 @@ from domain.cosine_similarity.TextProcessor import TextProcessor
 class TestFileRepository(unittest.TestCase):
 
     def setUp(self):
-        self.repository = FileRepository(path='../../resources/train.txt')
+        self.repository = FileRepository(train_path='../../resources/train.txt',
+                                         test_path='../../resources/test.txt')
 
     def test_get_from_repository(self):
         records = self.repository.get()
