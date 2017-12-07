9b48301f1282bfeb64b9d252350a3ebacf39d0ff
lore <lorenzomartino86@gmail.com>
2017-12-07 13:39:54 +0100

    added test coverage

--- src/domain/cosine_similarity/TextProcessor.py
+++ src/domain/cosine_similarity/TextProcessor.py
@@ -3,6 +3,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 class TextProcessor(object):
 
     def __init__(self, stop_words='english'):
+        """
+        Using TfIDF vectorizer that uses Tf (Term Frequency) and IDF (Inverse document frequency)
+                   tfidf(t, d, D) = tf(t, d) * idf(t, D)
+        where:
+            - tf(t, D) = count(t, d) / |d|
+            - idf(t, D) = log(|D| / |{d in D : t in d}|)
+        :param stop_words: by default 'english'
+        """
         self.vectorizer = TfidfVectorizer(stop_words=stop_words)
 
     def transform(self, records):
